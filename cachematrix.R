f67c214666b5a9115967006d1b23997990a528f1
serebe <sebastia.restrepo.betancur@gmail.com>
2020-05-21 16:12:20 -0500

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# # y luego establezca el valor resuelto "s" como nulo
+# # luego cambié cada referencia a "mean" para "resolver"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  e <- NULL
+  set <- function(l) {
+    x <<- l
+    e <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) e <<- solve
+  getsolve <- function() e
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##  permite obtener la matriz inversa
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("matrix inversa")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
